07d3a3505046a8bb519ae7c27ea9834611bdc518
Olumide Gbadegesin <olumeedee@gmail.com>
2023-03-18 23:59:39 +0100

    Incomplete work

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
+from .config.config import config_dict
+from flask_restx import Api
+from .auth.views import auth_namespace
+from .courses.views import  courses_namespace
 
 
+
+def create_app(config=config_dict['dev']):
+    app = Flask(__name__) 
+    
+    app.config.from_object(config)
+
+    api=Api(app)
+
+    api.add_namespace(auth_namespace, path='/auth')
+    api.add_namespace(courses_namespace, path='/courses')
+
+    return app
+    app.run(debug=True)
 
